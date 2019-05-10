10dca223ff136fb2d833a6b083535b4c4a314e8c
Meisuleen Kased <meisuleen@gmail.com>
2019-05-10 13:04:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     erb :say
   end
 
-  get '/saywords/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word].to_s
     @word2 = params[:word].to_s
     @word3 = params[:word].to_s
